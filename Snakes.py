d43ce55ae1fbe92a648bed3664dde191164ef576
JoeyXu7 <joeyxu@live.com>
2022-08-02 20:58:41 +0000

    Snakes (Joey)

--- Snakes.py
+++ Snakes.py
@@ -1,6 +1,21 @@
-def ends():
-    print("counting ends ...")
+def print_board(board):
+    print("\n")
+    for i in range(5):
+        if i%2 == 0:
+            print (".", board[i][0], ".", board[i][1], ".", board[i][2], ".")
+        else:
+            print (board[i][0], " ", board[i][1], " ", board[i][2], " ", board[i][3])
+    print("\n")
 
+def ends(board):
+    print("counting ends ...")
+    
+    for i in range (len(array_1)):
+        print (i)
+        for j in range(len(array_1[0])):
+         
+            
+  
 
 if __name__ == '__main__':
     print("Snakes")
